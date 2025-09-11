0f14d5b0aacc2079f48cd9e5f1d2d1e7f7ae2cf3
boslqx <bosliangqx@gmail.com>
2025-09-11 21:29:50 +0800

    create class Product with entitty and attributes

---
+++ Question1_Hashing/product.py
@@ -0,0 +1,17 @@
+# class Product stores baby product info of the retail store
+class Product:
+    def __init__(self, product_id, name, category, brand, price, stock_quantity):
+        self.product_id = product_id
+        self.name = name
+        self.category = category
+        self.brand = brand
+        self.price = price
+        self.stock_quantity = stock_quantity
+
+    def __str__(self):
+        return (f"ID: {self.product_id}, "
+                f"Name: {self.name}, "
+                f"Category: {self.category}, "
+                f"Brand: {self.brand}, "
+                f"Price: RM{self.price:.2f}, "
+                f"Stock: {self.stock_quantity}")
